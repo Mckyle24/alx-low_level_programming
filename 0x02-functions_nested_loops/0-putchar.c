cb6f7b69647d0b18a5d339b37d1ad3123eb53f22
Mckyle24 <84808695+Mckyle24@users.noreply.github.com>
2023-02-22 00:12:04 +0200

    Create 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes putchar
+ * The character to print
+ *
+ * Return: On success 0.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(void)
+{
+  printf('_putchar \n"
+	return (0);
+}
