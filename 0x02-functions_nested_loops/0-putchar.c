a0c1f25d225740daa69b6602e730160883249fa2
Mckyle24 <84808695+Mckyle24@users.noreply.github.com>
2023-02-22 00:32:21 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,23 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes putchar
- * The character to print
+ * main - Prints _putchar
+ * @void: Empty parameter list for main.
  *
- * Return: On success 0.
- * On error, -1 is returned, and errno is set appropriately.
- */
+ * Description: Prints _putchar w/out stdio
+ *
+ * Return: 0 for success
+*/
 int main(void)
 {
-  printf('_putchar \n"
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
